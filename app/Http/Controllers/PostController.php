1456aa0a36b04724d076fe2bfbd517a6d6d3c421
Julio Cesar Dalben Junior <juniordalben@mac.home>
2025-06-16 19:15:22 +0100

    Added middleware

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -27,6 +28,7 @@ class PostController extends Controller
     }
 
     public function viewSinglePost(Post $post) { //Type hinting. It will look the appropriate post in the db based on the incoming value
+        $post['body'] = strip_tags(Str::markdown($post->body), '<p><ul><ol><li><strong><em><h3>'); //overriding the body value with markdown
         return view('single-post', ['post' => $post]);
     }
 }
