fc64d416771823a09ba58ef3247083c9afe87c50
Julio Cesar Dalben Junior <juniordalben@mac.home>
2025-06-23 22:51:40 +0100

    File uploads etc

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -31,4 +31,30 @@ class PostController extends Controller
         $post['body'] = strip_tags(Str::markdown($post->body), '<p><ul><ol><li><strong><em><h3>'); //overriding the body value with markdown
         return view('single-post', ['post' => $post]);
     }
+
+    public function delete(Post $post) {
+        
+        $post->delete();
+
+        return redirect('/profile/' . auth()->user()->username)->with('success', 'Post succesfully deleted.');
+    }
+
+    public function showEditForm(Post $post) {
+        return view('edit-post', ['post' => $post]);
+    }
+
+   
+    public function actuallyUpdate(Post $post, Request $request) {
+        $incomingFields = $request->validate([
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+
+        $incomingFields['title'] = strip_tags($incomingFields['title']); // stripping out html/javascript tags
+        $incomingFields['body'] = strip_tags($incomingFields['body']);
+
+        $post->update($incomingFields);
+
+        return back()->with('success', 'Post successfully updated.'); //Takes user back to the edit post form
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,22 @@ Route::get('/', [UserController::class, "showCorrectHomepage"])->name('login');
 Route::post('/register', [UserController::class, 'register'])->middleware('guest');
 Route::post('/login', [UserController::class, 'login'])->middleware('guest');
 Route::post('/logout', [UserController::class, 'logout'])->middleware('mustBeLoggedIn');
+Route::get('/manage-avatar', [UserController::class, 'showAvatarForm']);
+Route::post('/manage-avatar', [UserController::class, 'storeAvatar']);
 
 // Blog post related routes
 Route::get('/create-post', [PostController::class, 'showCreateForm'])->middleware('mustBeLoggedIn'); //Intercepts the request and runs if func only if user is logged in
 Route::post('/create-post', [PostController::class, 'storeNewPost'])->middleware('mustBeLoggedIn');
 Route::get('/post/{post}', [PostController::class, 'viewSinglePost']);
+Route::delete('/post/{post}', [PostController::class, 'delete'])->middleware('can:delete,post');
+Route::get('/post/{post}/edit', [PostController::class, 'showEditForm'])->middleware('can:update,post');
+Route::put('/post/{post}', [PostController::class, 'actuallyUpdate'])->middleware('can:update,post')->middleware('can:update,post');
+
 
 // Profile related routes
 Route::get('/profile/{user:username}', [UserController::class, 'profile']);
+
+//Admin route
+Route::get('/admins-only', function() {
+    return 'Only admins can see this page';
+})->middleware('can:visitAdminPages');
